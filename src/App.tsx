cc754ba3d0b9b634d9fb227e0e325dfa61fc6b7d
Saul Lee <solleedata@gmail.com>
2025-02-11 18:52:40 +0900

    refactor: Move main application logic to Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,7 @@
-import { useState } from 'react';
-import { APPLICATION_NAME } from './constants';
-import PurchaseAmountInput from './features/purchase/UI/PurchaseAmountInput';
-import Header from './components/Header';
-import { DrawList } from './features/draw/UI/Draw';
+import Main from './pages/main';
 
 function App() {
-  const [drawAmount, setDrawAmount] = useState(0);
-
-  return (
-    <div
-      style={{
-        padding: '36px 24px',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '12px',
-        width: '500px',
-        margin: '0 auto',
-      }}
-    >
-      <Header size='large' fontWeight='bold'>
-        {APPLICATION_NAME}
-      </Header>
-
-      <PurchaseAmountInput
-        onAmountConfirm={(amount) => setDrawAmount(+amount / 1000)}
-      />
-
-      {drawAmount > 0 && <DrawList amount={drawAmount} />}
-    </div>
-  );
+  return <Main />;
 }
 
 export default App;
---
+++ src/pages/main.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { APPLICATION_NAME } from '../constants';
+import PurchaseAmountInput from '../features/purchase/UI/PurchaseAmountInput';
+import Header from '../components/Header';
+import { DrawList } from '../features/draw/UI/Draw';
+
+export default function Main() {
+  const [drawAmount, setDrawAmount] = useState(0);
+
+  return (
+    <div
+      style={{
+        padding: '36px 24px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '12px',
+        width: '500px',
+        margin: '0 auto',
+      }}
+    >
+      <Header size='large' fontWeight='bold'>
+        {APPLICATION_NAME}
+      </Header>
+
+      <PurchaseAmountInput
+        onAmountConfirm={(amount) => setDrawAmount(+amount / 1000)}
+      />
+
+      {drawAmount > 0 && <DrawList amount={drawAmount} />}
+    </div>
+  );
+}

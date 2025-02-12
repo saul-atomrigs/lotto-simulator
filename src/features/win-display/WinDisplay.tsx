186a1572b43b01b119b245e916b07925577f228e
Saul Lee <solleedata@gmail.com>
2025-02-12 12:22:58 +0900

    feat: Enhance WinDisplay to include a title for winning numbers

--- src/features/win-display/WinDisplay.tsx
+++ src/features/win-display/WinDisplay.tsx
@@ -1,8 +1,16 @@
-import { Box } from '../../components';
+import { Box, Txt } from '../../components';
 import { useWinStore } from '../../store/winStore';
 import { sortElements } from '../../utils';
 
 export default function WinDisplay() {
   const winningNumbers = useWinStore((state) => state.winningNumbers);
-  return <Box type='winner'>{sortElements(winningNumbers)}</Box>;
+
+  return (
+    <>
+      <Txt size='small' fontWeight='bold'>
+        당첨 번호
+      </Txt>
+      <Box type='winner'>{sortElements(winningNumbers)}</Box>
+    </>
+  );
 }
